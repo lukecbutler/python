fdd6d85e6e9588d617d3c800f19caf8bcbda7003
tarheelfan200 <lukechristopherbutler@gmail.com>
2026-03-04 09:18:27 -0500

    push leetcode problem

---
+++ NeetCode150/ContainsDuplicates.py
@@ -0,0 +1,28 @@
+'''
+Contains Duplicate
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: false
+'''
+
+def hasDuplicate(nums: list[int]) -> bool:
+    mySet = set()
+    for i in nums:
+        if i in mySet:
+            return True
+        else:
+            mySet.add(i)
+
+    return False
+
+
+if __name__ == "__main__":
+    nums = [1,2,3,4]
+    print(hasDuplicate(nums))
